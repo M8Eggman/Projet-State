489b2f0a0c8ee71abdd49a9c0327a185b31be8d4
GoNEAR <cagatay.morgul@outlook.com>
2025-05-21 14:48:25 +0200

    logique form info fini

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -7,14 +7,27 @@ import Titre from "../titre/Titre";
 
 function Info(props) {
   const [focus, setFocus] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
   return (
     <>
       <div className="info">
         <Titre titre={"Personal Info"} paragraphe={"Please provide your name, email address, and phone number."} />
         <form className="infoForm" action="">
-          <label htmlFor="infoName">Your name is: {}</label>
+          <label htmlFor="infoName">Your name is: {name}</label>
           <div>
-            <input type="text" name="" id="infoName" placeholder="Vingt-Six" onFocus={() => setFocus("infoName")} onBlur={() => setFocus("")} />
+            <input
+              type="text"
+              name=""
+              id="infoName"
+              placeholder="Vingt-Six"
+              onFocus={() => setFocus("infoName")}
+              onBlur={() => setFocus("")}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
             {focus === "infoName" && (
               <div className="inputIcon">
                 <div className="userIcon">
@@ -24,9 +37,19 @@ function Info(props) {
               </div>
             )}
           </div>
-          <label htmlFor="infoEmail">Your email is: {}</label>
+          <label htmlFor="infoEmail">Your email is: {email}</label>
           <div>
-            <input type="text" name="" id="infoEmail" placeholder="vingt_six@email.com" onFocus={() => setFocus("infoEmail")} onBlur={() => setFocus("")} />
+            <input
+              type="text"
+              name=""
+              id="infoEmail"
+              placeholder="vingt_six@email.com"
+              onFocus={() => setFocus("infoEmail")}
+              onBlur={() => setFocus("")}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
             {focus === "infoEmail" && (
               <div className="inputIcon">
                 <div className="userIcon">
@@ -36,9 +59,20 @@ function Info(props) {
               </div>
             )}
           </div>
-          <label htmlFor="infoPhone">Your phone number is: {}</label>
+          <label htmlFor="infoPhone">Your phone number is: {phone}</label>
           <div>
-            <input type="number" name="" id="infoPhone" min={0} placeholder="e.g.+1 234 567 890" onFocus={() => setFocus("infoPhone")} onBlur={() => setFocus("")} />
+            <input
+              type="number"
+              name=""
+              id="infoPhone"
+              min={0}
+              placeholder="e.g.+1 234 567 890"
+              onFocus={() => setFocus("infoPhone")}
+              onBlur={() => setFocus("")}
+              onChange={(e) => {
+                setPhone(e.target.value);
+              }}
+            />
             {focus === "infoPhone" && (
               <div className="inputIcon">
                 <div className="userIcon">
@@ -49,7 +83,7 @@ function Info(props) {
             )}
           </div>
         </form>
-        <DivBouton etape={props.etape} setEtape={props.setEtape}/>
+        <DivBouton etape={props.etape} setEtape={props.setEtape} />
       </div>
     </>
   );
